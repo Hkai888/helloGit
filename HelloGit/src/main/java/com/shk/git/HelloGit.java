c6344adb54b6ad7b2af8ef922312a31ff276d243
Hkai <1198852645@qq.com>
2020-05-22 22:21:00 +0800

    添加文件

---
+++ HelloGit/src/main/java/com/shk/git/HelloGit.java
@@ -0,0 +1,11 @@
+package com.shk.git;
+
+/**
+ * @Author: Hkai
+ * @Date: 2020/5/22 22:08
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
